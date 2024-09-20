eb5f2fe4c2fded7b2f2294254b040db462b50fac
larads <dasilval.mariana@gmail.com>
2024-09-20 15:40:25 -0400

    ✨ feat: criando página de sucesso com retornos dos dados da API do stripe e Redirect dentro do ServerSideRendering quando parâmetro inválido.

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -8,6 +8,7 @@ import { stripe } from "@/lib/stripe"
 import { ImageContainer, Loading, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useState } from "react"
 import axios from "axios"
+import Head from "next/head"
 
 interface ProductProps {
     product: {
@@ -49,23 +50,29 @@ export default function Product({ product }: ProductProps) {
     }
 
     return (
-        <ProductContainer>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
-            </ImageContainer>
+        <>
+            <Head>
+                <title>{product.name} | Ignite Shop</title>
+            </Head>
 
-            <ProductDetails>
-                <h1>{product.name}</h1>
-                <span>{product.price}</span>
+            <ProductContainer>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={520} height={480} alt="" />
+                </ImageContainer>
 
-                <p>{product.description}</p>
+                <ProductDetails>
+                    <h1>{product.name}</h1>
+                    <span>{product.price}</span>
+
+                    <p>{product.description}</p>
 
 
-                <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
-                    Comprar agora
-                </button>
-            </ProductDetails>
-        </ProductContainer>
+                    <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
+                        Comprar agora
+                    </button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     )
 }
 
---
+++ src/pages/sucess/index.tsx
@@ -0,0 +1,76 @@
+import Stripe from "stripe";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import { GetServerSideProps } from "next";
+
+import { stripe } from "@/lib/stripe";
+import { ImageContainer, SuccessContainer } from '@/styles/pages/sucess'
+
+interface SuccessProps {
+    customerName: string
+    product: {
+        name: string
+        imageUrl: string
+    }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
+
+    return (
+        <>
+            <Head>
+                <title>Compra efetuada | Ignite Shop</title>
+
+                <meta name="robots" content="noindex" />
+            </Head>
+
+            <SuccessContainer>
+                <h1>Compra efeturada!</h1>
+
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={120} height={110} alt="" />
+                </ImageContainer>
+
+                <p>
+                    Uhuuul <strong>{customerName}</strong>, Sua camiseta <strong>{product.name}</strong> já está a caminhao da sua casa
+                </p>
+                <Link href="/">
+                    Voltar ao catálogo
+                </Link>
+            </SuccessContainer>
+        </>
+    )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    if (!query.session_id) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    const sessionId = String(query.session_id)
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        expand: ['line_items', 'line_items.data.price.product']
+    })
+
+
+    const customerName = session.customer_details.name
+    const product = session.line_items.data[0].price.product as Stripe.Product
+
+    return {
+        props: {
+            customerName,
+            product: {
+                name: product.name,
+                imageUrl: product.images[0]
+            }
+        }
+    }
+}
