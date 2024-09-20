716d25f98e327aa31977a6e64f1694b68a81c905
larads <dasilval.mariana@gmail.com>
2024-09-20 14:12:33 -0400

    ✨ feat: Formatting data coming from the STRIPE API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,15 +45,21 @@ export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
+
   const products = response.data.map(product => {
     const price = product.default_price as Stripe.Price
+
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount! / 100)
     }
   })
+
   return {
     props: {
       products
