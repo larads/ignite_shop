4ca9c2dcb3026a115d8c7130331d0309260fa639
larads <dasilval.mariana@gmail.com>
2024-09-20 10:37:34 -0400

    ✨ feat: creating product cards

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import { Roboto_Flex as Roboto } from 'next/font/google'
 import type { AppProps } from "next/app";
+import Image from 'next/image';
 
+import { globalStyles } from '@/styles/globals';
+import { Container, Header } from '@/styles/pages/app';
+
+import Logo from '../assets/Logo.svg'
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={Logo} alt='' />
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,49 @@
+import Image from 'next/image';
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
+import { HomeContainer, Product } from '@/styles/pages/home'
+
+import camista1 from '../assets/Shirt/1.png'
+import camista2 from '../assets/Shirt/2.png'
+import camista3 from '../assets/Shirt/3.png'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <h1>oi</h1>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
+        <Image src={camista1} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camista2} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camista3} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
