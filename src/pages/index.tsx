b39bb5aee9fbc7b40d30dac731fbac0430686c9f
larads <dasilval.mariana@gmail.com>
2024-09-20 14:54:41 -0400

    ✨ feat: Creating product view page layout and loading project data via (Static Props), generating StaticPaths during build.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Stripe from "stripe"
+import Link from "next/link"
 import Image from 'next/image';
 import { GetStaticProps } from "next"
 import 'keen-slider/keen-slider.min.css'
@@ -12,7 +13,7 @@ interface HomeProps {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -28,13 +29,21 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+          >
+            <Product
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,85 @@
+import Stripe from "stripe"
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { CircularProgress } from "@mui/material"
+import { GetStaticPaths, GetStaticProps } from "next"
 
-export default function Product() {
-    const { query } = useRouter()
+import { stripe } from "@/lib/stripe"
+import { ImageContainer, Loading, ProductContainer, ProductDetails } from "@/styles/pages/product"
+
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+    }
+}
+
+export default function Product({ product }: ProductProps) {
+    const { isFallback } = useRouter()
+
+    if (isFallback) {
+        return (
+            <Loading>
+                <CircularProgress color="success" size={50} />
+            </Loading>
+        )
+    }
 
     return (
-        <h1>Product {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+
+                <p>{product.description}</p>
+
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { id: 'prod_O5YuClun0Kf64y' } }
+        ],
+        fallback: true
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price']
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(price.unit_amount! / 100),
+                description: product.description
+            }
+        },
+        revalidate: 60 * 60 * 1,
+    }
 }
